cb3a0cc6a489159a18845ebb3084c7b6f0684bd4
Gerard Braad <me@gbraad.nl>
2016-08-18 07:41:23 +0000

    Move to versioned image

--- compose.sh
+++ compose.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pushd /home/working
+pushd /workspace
 
 git clone https://github.com/CentOS/sig-atomic-buildscripts \
     -b downstream \
@@ -11,11 +11,6 @@ rpm-ostree compose tree \
     --repo=/srv/repo \
     ./centos-atomic/centos-atomic-host.json
 
-#find /srv/repo/ -type f | ./genindex.sh > /srv/repo/index.html
-cat << EOF > /srv/repo/index.html
-<html><body>
-<a href="https://gitlab.com/gbraad/byo-atomic-centos/">byo-atomic-centos</a>
-</body></html>
-EOF
+rm -rf /srv/repo/tmp
 
 popd
